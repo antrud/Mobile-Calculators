90ab4fb274ba73203c56631132ae19226917a4b8
Anton Rudashko <anton.rudashko@welltech.com>
2023-01-08 20:06:36 +0200

    changed README

--- src/main/java/WebMainPage.java
+++ src/main/java/WebMainPage.java
@@ -2,29 +2,73 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
 
-public class HybridMainPage {
+public class WebMainPage {
 
     AppiumDriver<MobileElement> driver;
 
-    @AndroidFindBy(id = "button2")
-    private MobileElement btnDecline;
+    @AndroidFindBy(id = "terms_accept")
+    private MobileElement btnChromeTermsAccept;
 
-    @AndroidFindBy
-    private MobileElement btnTwo;
+    @AndroidFindBy(id = "negative_button")
+    private MobileElement btnDeclineSync;
+
+    @AndroidFindBy(id = "search_box_text")
+    private MobileElement etChromeSearch;
+
+    @AndroidFindBy(id = "line_2")
+    private MobileElement btnGotoUrl;
+
+    @FindBy(xpath = "//span[contains(@class, 'scinm') and text() = '2']")
+    private WebElement btnTwo;
+
+    @FindBy(xpath = "//span[contains(@class, 'sciop') and text() = '+']")
+    private MobileElement btnPlus;
+
+    @FindBy(xpath = "//*[@id='sciOutPut']")
+    private MobileElement result;
 
 
-    public void declineTwitter() {
-        btnDecline.click();
+    public void acceptChromeTerms() {
+        if (btnChromeTermsAccept.isDisplayed()) {
+            btnChromeTermsAccept.click();
+        }
+    }
+
+    public void declineChromeSync() {
+        if (btnDeclineSync.isDisplayed()) {
+            btnDeclineSync.click();
+        }
+    }
+
+    public void getUrl(String url) {
+        etChromeSearch.sendKeys(url);
+        btnGotoUrl.click();
+    }
+
+    public void clickTwo() {
+        btnTwo.click();
+    }
+
+    public void clickPlus() {
+        btnPlus.click();
+    }
+
+    public String getResult() {
+        String firstResult = result.getText();
+        return firstResult.replaceAll("\\D", "");
     }
 
 
 
-    public HybridMainPage(AppiumDriver<MobileElement> driver) {
+    public WebMainPage(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(5)), this);
     }
+
 }
--- src/test/java/WebCalcTest.java
+++ src/test/java/WebCalcTest.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class WebCalcTest {
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+
+public class WebCalcTest {
+
+    private AppiumDriver<MobileElement> driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+//        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:appPackage", "com.android.chrome");
+        desiredCapabilities.setCapability("appium:appActivity", "com.google.android.apps.chrome.Main");
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+        desiredCapabilities.setCapability("appium:autoGrantPermissions", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AppiumDriver<>(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+
+//        driver.get("https://calculator-online.net/");
+//        Thread.sleep(3_000);
+
+        WebMainPage page = new WebMainPage(driver);
+        page.acceptChromeTerms();
+        page.declineChromeSync();
+        page.getUrl("https://www.calculator.net/");
+        Thread.sleep(3_000);
+
+        driver.context("WEBVIEW_chrome");
+
+        page.clickTwo();
+        page.clickPlus();
+        page.clickTwo();
+
+        String actualResult = page.getResult();
+
+        Assert.assertEquals(actualResult, "4");
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
 }
