82d8ab97b722fa39a2c19bf9393a01045088c573
Anton Rudashko <arudashko@gmail.com>
2023-02-28 18:19:01 +0200

    changed to Browserstack

--- src/test/java/NativeCalcTest.java
+++ src/test/java/NativeCalcTest.java
@@ -16,19 +16,33 @@ public class NativeCalcTest {
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
-        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
-        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
-        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
-        desiredCapabilities.setCapability("appium:appPackage", "com.google.android.calculator");
-        desiredCapabilities.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
-        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("platformName", "android");
+//        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
+//        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+//        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+//        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+//        desiredCapabilities.setCapability("appium:appPackage", "com.google.android.calculator");
+//        desiredCapabilities.setCapability("appium:appActivity", "com.android.calculator2.Calculator");
+//        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+//
+//        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+//
+//        driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
 
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
+        capabilities.setCapability("device", "Samsung Galaxy S22 Ultra");
+        capabilities.setCapability("os_version", "12.0");
+        capabilities.setCapability("project", "First TestNG Android Project");
+        capabilities.setCapability("build", "rowserstack-build-1");
+        capabilities.setCapability("name", "first_test");
+        capabilities.setCapability("browserstack.debug", true);
+        capabilities.setCapability("app", "bs://aa0598c925cfe1a50b098f05ef43092f425ec987");
+
+        driver = new AndroidDriver (
+                new URL("http://antonr_RdnaX2:6HxNtVmkBJPSUS8ng2UJ@hub-cloud.browserstack.com/wd/hub"), capabilities
+        );
     }
 
     @Test
